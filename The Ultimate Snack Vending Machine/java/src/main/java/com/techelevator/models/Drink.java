c45a91f23b5e7b10d88e218e4ec561332d23831b
developerAlida <94207769+developerAlida@users.noreply.github.com>
2021-12-07 15:40:56 -0500

    Add files via upload

---
+++ The Ultimate Snack Vending Machine/java/src/main/java/com/techelevator/models/Drink.java
@@ -0,0 +1,17 @@
+package com.techelevator.models;
+
+public class Drink extends Items{
+    public Drink(String itemType, String itemSound) {
+        super("Drink", "Glug, glug, yum!");
+    }
+
+    @Override
+    public String getItemType() {
+        return super.getItemType();
+    }
+
+    @Override
+    public String getItemSound() {
+        return super.getItemSound();
+    }
+}
